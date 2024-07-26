05cd8d1d3dd55002527eb92ea42b6267d7beeaaa
Quarantining <165849060+Quarantining@users.noreply.github.com>
2024-07-26 12:56:15 -0700

    Update basic-phonebook.py
    made it so user cannot add a name if it already exists in the phonebook. added confirmation when deleting a name to avoid accidents. added more comments. added some basic error handling.  added notes and future plans as comments at the top.

--- basic-phonebook.py
+++ basic-phonebook.py
@@ -5,14 +5,28 @@
 
 # list of things to add:
     # more default phonebook entries (done)
-    # more descriptive comments
+    # more descriptive comments (on going)
     # error handling
     # when deleting an entry, show the phone number. Ex) "Removed Bob "123""
     # actually remember to update the version number
+    # allow for non casesensitive searching/deleting
+    # confirmation when deleting entry
+    # save and load data to file
+    # edit existing entries
+    # help command
+    # clear main menu after each use for cleaner interaction
+    # reformat program into using functions
 
+# completed:
+    # made it not possible to add duplicate names.
+    # added confirmation when deleting a name.
+
+##############################################################
+
+# simple phonebook program
 
 # displays version on title screen
-version = "1.1"
+version = "1.2"
 
 # entries
 phonebook = {
@@ -45,31 +59,45 @@ basic phonebook
 while True:
     # user selection
     user_input = input("> ")
+    try:
+        # search name
+        if user_input == "search":
+            search = input(str("enter name --> ")) # prompts user to enter name to search for
+            print(phonebook.get(search, "entry not found")) # prints entry if found
 
-    # search name
-    if user_input == "search":
-        search = input(str("enter name --> ")) # prompts user to enter name to search for
-        print(phonebook.get(search, "entry not found")) # prints entry if found
+        # adds entry
+        elif user_input == "add":
+            name = input("enter new name: ") # adds key
+            number = input("enter new number: ") # adds value
+            if name in phonebook: # checks if new name already exists.
+                print(f"{name} already exists in phonebook.") # tells user their name already exists.
+            else:
+                phonebook[name] = number # adds key and value using vars above
+                print(f"entry added: {name} --> {number}.") # prints new entry
 
-    # adds entry
-    elif user_input == "add":
-        name = input("enter new name: ") # adds key
-        number = input("enter new number: ") # adds value
-        phonebook[name] = number # adds key and value using vars above
-        print(f"entry added: {name} --> {number}.") # prints new entry
+        # deletes an entry
+        elif user_input == "delete":
+           name = (input("enter name to delete: ")) # name to remove
+           if name in phonebook:
+               confirm = input(f"delete {name}? (yes/no): ") # makes sure user wants to delete name.
+               if confirm == "yes": # "yes" --> deletes
+                   del phonebook[name] # removes name from phonebook
+                   print(f"{name} removed.") # tells user name was removed
+               elif confirm == "no": # "no" --> action cancled.
+                   print(f"{name} was not removed.")
+               else:
+                   print("invalid input.") # invalid input
 
-    # deletes an entry
-    elif user_input == "delete":
-       name = (input("enter name to delete: ")) # name to remove
-       del phonebook[name] # removes name from phonebook
-       print(f"{name} removed.") # tells user name was removed
+        # shows all entries
+        elif user_input == "list":
+            print(phonebook)
 
-    # shows all entries
-    elif user_input == "list":
-        print(phonebook)
-
-    # stops program
-    elif user_input == "stop":
-        break
-    else:
-        print("Invalid Input. Please try again.")
+        # stops program
+        elif user_input == "stop":
+            break
+        else:
+            print("Invalid Input. Please try again.")
+    except KeyError:
+        print("KeyError")
+    except ValueError:
+        print("ValueError")

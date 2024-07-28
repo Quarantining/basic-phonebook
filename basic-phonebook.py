fa8c23607cac2e9f2137b2d1080cd02acd73af1b
Quarantining <165849060+Quarantining@users.noreply.github.com>
2024-07-27 20:46:39 -0700

    Update basic-phonebook.py
    reformatted code. moved most commands to functions. code is easier to read and there are more descriptive comments.

--- basic-phonebook.py
+++ basic-phonebook.py
@@ -1,9 +1,9 @@
 # simple phonebook program
 
 # displays version on title screen
-version = "1.4"
+version = "1.5"
 
-# entries
+# phonebook entries
 phonebook = {
     "Yuki Tanaka": "555-1234",
     "Minji Kim": "555-5678",
@@ -18,7 +18,7 @@ phonebook = {
 }
 
 
-# title screen
+# prints title screen
 print(f"""- - - - - - - - - - - - - - - - -
 basic phonebook             
 
@@ -31,64 +31,119 @@ basic phonebook
 - - - - - - - - - - - - - - - - -
 """)
 
+
+# searches for key value (name).
+def search_name(search):
+    print(phonebook.get(search, "entry not found")) # prints entry (if found) otherwise says it cannot locate it.
+
+
+# doesn't work.
+def search_num():
+    print("currently unavailable") # this is a work in progress so it's not available yet.
+
+
+# allows user to add entry. elif to prevent duplicate names.
+def add_entries(name, number):
+    if name in phonebook:  # checks if new name already exists.
+        print(f"{name} already exists in phonebook.")  # tells user their name already exists.
+    else:
+        phonebook[name] = number  # adds key and value using vars above
+        print(f"entry added: {name} --> {number}.")  # prints new entry
+
+
+# allows user to delete entries.
+def delete_entries(name):
+    if name in phonebook:
+        confirm = input(f"delete {name}? (yes/no): ")  # makes sure user wants to delete name.
+        if confirm == "yes":  # "yes" --> deletes.
+            del phonebook[name]  # removes name from phonebook.
+            print(f"{name} removed.")  # tells user the name was removed.
+        elif confirm == "no":  # "no" --> action canceled.
+            print(f"{name} was not removed.")
+        else:
+            print("invalid input.")  # invalid input.
+
+
+# allows user to list all entries
+def list_entries():
+    # Calculate the longest name length
+    longest_name = max(len(name) for name in phonebook)  # finds longest name
+    # Show all entries
+    for key, value in phonebook.items():
+        # Calculate the number of dots needed
+        dot = "." * (longest_name - len(key) + 10)  # calculates the difference
+        print(f"{key}{dot}{value}")
+
+
+
+
+
 while True:
-    # user selection
+
     user_input = input("> ")
+
     try:
+
         # search name
         if user_input == "search":
+
             search_type = input("how do you want to search? (name/number): ")
-            
+
+            # checks for key value (name).
             if search_type == "name":
                 search = input(str("enter name --> ")) # prompts user to enter name to search for
-                print(phonebook.get(search, "entry not found")) # prints entry if found
+                search_name(search)
+
+            # checks for value (number).
+            elif search_type == "number":
+                search_num()
+
 
         # adds entry
         elif user_input == "add":
-            name = input("enter new name: ") # adds key
-            number = input("enter new number: ") # adds value
-            if name in phonebook: # checks if new name already exists.
-                print(f"{name} already exists in phonebook.") # tells user their name already exists.
-            else:
-                phonebook[name] = number # adds key and value using vars above
-                print(f"entry added: {name} --> {number}.") # prints new entry
+            name = input("enter new name: ")  # adds key
+            number = input("enter new number: ")  # adds value
+            add_entries(name, number) # calls function
 
         # deletes an entry
         elif user_input == "delete":
-           name = (input("enter name to delete: ")) # name to remove
-           if name in phonebook:
-               confirm = input(f"delete {name}? (yes/no): ") # makes sure user wants to delete name.
-               if confirm == "yes": # "yes" --> deletes
-                   del phonebook[name] # removes name from phonebook
-                   print(f"{name} removed.") # tells user name was removed
-               elif confirm == "no": # "no" --> action cancled.
-                   print(f"{name} was not removed.")
-               else:
-                   print("invalid input.") # invalid input
+            name = (input("enter name to delete: ")) # name to remove
+            delete_entries(name) # calls function
 
         # shows all entries
         elif user_input == "list":
-            # Calculate the longest name length
-            longest_name = max(len(name) for name in phonebook) # finds longest name
-            # Show all entries
-            for key, value in phonebook.items():
-                # Calculate the number of dots needed
-                dot = "." * (longest_name - len(key) + 10) # calculates the difference
-                print(f"{key}{dot}{value}")
+            list_entries() # calls function (no argument needed).
 
         # stops program
         elif user_input == "stop":
-            confirm_stop = input("are you sure? (yes/no): ")
+            confirm_stop = input("are you sure? (yes/no): ") # stop confirmation
+
             if confirm_stop == "yes":
                 print("stopping..")
-                break
+                break # ends program
+
             elif confirm_stop == "no":
                 print("")
+
             else:
-                print("Invalid Input. Please try again.")
+                print("Invalid Input. Please try again.") # if user enters invalid input.
+
         else:
-            print("Invalid Input. Please try again.")
+            print("Invalid Input. Please try again.") # if user enters invalid input.
+    # to avoid errors.
     except KeyError:
         print("KeyError")
     except ValueError:
         print("ValueError")
+
+
+
+
+
+# list of things to add:
+    # better error handling.
+    # allow for upper/lower inputs.
+    # way to edit an existing entry.
+    # clear main menu for better interaction.
+    # once a mode is selected, loop it until user either completes an action or exist. (this way it wont
+    # exit even after you input a invalid value).
